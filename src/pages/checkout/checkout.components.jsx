08707d99c7395f9e27a7525cae8c0993afe9f68e
lancelot03001 <pktsangac@connect.ust.hk>
2021-02-03 16:05:40 +0800

    updatied stripe integration and updated favicon and title

--- src/pages/checkout/checkout.components.jsx
+++ src/pages/checkout/checkout.components.jsx
@@ -5,6 +5,7 @@ import {createStructuredSelector} from 'reselect'
 import {selectCartItems, selectCartTotal} from '../../redux/cart/cart.selectors'
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component'
 
 import './checkout.styles.scss'
 
@@ -36,6 +37,13 @@ const CheckoutPage = ({cartItems, totalPrice}) => (
         <div className='total'>
             <span>Total: ${totalPrice}</span>
         </div>
+
+        <div className='test-warning'>
+            *Please use the following test credit card for payments*
+            <br/>
+            4242 4242 4242 4242 - Exp: any future date, - CVV: any 3 digits
+        </div>
+        <StripeCheckoutButton price={totalPrice} />
     </div>
 )
 
